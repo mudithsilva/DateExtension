cb38b0078ec4534e75ca9b011e3da494a2643a6e
Mudith Chathuranga <mudithchathuranga@gmail.com>
2018-06-24 19:35:17 +0530

    Initial Commit

---
+++ DateExtension.swift
@@ -0,0 +1,55 @@
+//
+//  DateExtension.swift
+//  MYP
+//
+//  Created by Mudith Chathuranga on 6/15/18.
+//  Copyright © 2018 Chathuranga. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    var yesterday: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    
+    var tomorrow: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    
+    var isLastDayOfMonth: Bool {
+        return tomorrow.month != month
+    }
+    
+    func tokenOutDate(minutes: Int) -> Date {
+        return Calendar.current.date(byAdding: .minute, value: minutes, to: Date())!
+    }
+    
+    func monthsBack(months: Int) -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: -months, day: 0), to: self)!
+    }
+    
+    func startOfYear() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year], from: Calendar.current.startOfDay(for: self)))!
+    }
+    
+    func endOfYear() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(year: 1, day: -1), to: self.startOfYear())!
+    }
+    
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+    
+    func endOfMonth() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
+    }
+}
